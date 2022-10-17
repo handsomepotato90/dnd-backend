648764bf7bc75a7ccd7f8ee45a6b03c7b31538fc
Handsomepotato <tsvetan46@gmail.com>
2022-10-17 16:53:05 +0300

    added name search to My profile

--- routes/myProfile.js
+++ routes/myProfile.js
@@ -6,7 +6,10 @@ const { default: mongoose } = require("mongoose");
 
 router.post("/", async (req, res, next) => {
   try {
-    monster = await Monster.find({creator: req.body.user }).limit(req.body.limit);
+    monster = await Monster.find({
+      name: { $regex:req.body.name, $options: "i" },
+      creator: req.body.user,
+    }).limit(req.body.limit);
   } catch (err) {
     const error = new HttpError(
       `Can't fetch all creatures. Try again later.`,
@@ -17,42 +20,31 @@ router.post("/", async (req, res, next) => {
   res.status(201).json(monster);
 });
 
-
-
 router.delete("/:id", async (req, res, next) => {
-    try {
-      delEnc = await Monster.findById(req.params.id).populate('creator');
-    } catch (err) {
-      const error = new HttpError(
-        `Delete failed.`,
-        500
-      );
-      return next(error);
-    }
-    if(!delEnc){
-      const error = new HttpError(
-        `Can't find Creature.`,
-        500
-      );
-      return next(error);
-    }
+  try {
+    delEnc = await Monster.findById(req.params.id).populate("creator");
+  } catch (err) {
+    const error = new HttpError(`Delete failed.`, 500);
+    return next(error);
+  }
+  if (!delEnc) {
+    const error = new HttpError(`Can't find Creature.`, 500);
+    return next(error);
+  }
 
-    try {
-      const sess = await mongoose.startSession();
-      sess.startTransaction();
-      await delEnc.remove({session:sess});
-      delEnc.creator.monsters.pull(delEnc)
-      await delEnc.creator.save({session:sess});
-      await sess.commitTransaction();
-    } catch (err) {
-      const error = new HttpError(
-        `Delete failed`,
-        500
-      );
-      return next(error);
-    }
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await delEnc.remove({ session: sess });
+    delEnc.creator.monsters.pull(delEnc);
+    await delEnc.creator.save({ session: sess });
+    await sess.commitTransaction();
+  } catch (err) {
+    const error = new HttpError(`Delete failed`, 500);
+    return next(error);
+  }
 
-  res.status(201).json({message:'Encounter deleted.'});
+  res.status(201).json({ message: "Encounter deleted." });
 });
 
 module.exports = router;
