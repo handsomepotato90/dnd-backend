e5992d3b4ffa14776968d8c00dbd02b675e39c07
Handsomepotato <tsvetan46@gmail.com>
2023-09-19 15:10:00 +0300

    deploy of full version of site

--- controllers/checkAuth.js
+++ controllers/checkAuth.js
@@ -1,7 +1,7 @@
 const HttpError = require("../models/http-error");
 const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
-  console.log(req);
+
 
   if (req.method === "OPTIONS") {
     return next();
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
-      throw new Error("Authentication failed!");
+      throw new Error("Authentication failed!",402);
     }
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     req.userData = { userId: decodedToken.userId };
--- routes/Edit.js
+++ routes/Edit.js
@@ -4,9 +4,6 @@ const Monster = require("../models/monsters");
 const router = express.Router();
 const ckeckAuth = require("../controllers/checkAuth");
 
-
-
-
 router.get("/:id", async (req, res, next) => {
   const monsterId = req.params.id;
   try {
@@ -30,7 +27,7 @@ router.patch("/:id", async (req, res, next) => {
     const error = new HttpError(`Can't update monsters.`, 401);
     return next(error);
   }
-  if(monster.creator.toString() !== req.userData.userId){
+  if (monster.creator.toString() !== req.userData.userId) {
     const error = new HttpError(`You are Not allowed to edit this place.`, 401);
     return next(error);
   }
@@ -58,6 +55,5 @@ router.patch("/:id", async (req, res, next) => {
 
   res.status(201).json(monster);
 });
-
 
 module.exports = router;
